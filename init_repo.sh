700e66bdd2f3e75f65f84ff9f2a99df2a4f52437
Thomas Versteeg <thomasversteeg@gmx.com>
2020-04-05 18:07:24 +0200

    Fix replacement bug

--- init_repo.sh
+++ init_repo.sh
@@ -9,7 +9,7 @@ REPO_NAME=$(basename `git rev-parse --show-toplevel`)
 echo "Replacing project name in files with: \"$REPO_NAME\""
 
 # Replace the name
-git grep -l '*' | xargs sed -i -e "s/replace_me/$REPO_NAME/g"
+git ls-files | xargs sed -i -e "s/replace_me/$REPO_NAME/g"
 
 # Remove this script
 rm init_repo.sh
